d0fcb190d077f4f314d455939102c60bf01d7d0f
Damien Maillard <damien.maillard@dailymotion.com>
2017-10-19 17:04:40 +0200

    externalize test runner into @dmail/test

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 // https://github.com/jsenv/core/blob/959e76068b62c23d7047f6a8c7a3d6582ac25177/more/lib/instruction/util/object-equals.js#L3
 // https://github.com/jsenv/core/tree/959e76068b62c23d7047f6a8c7a3d6582ac25177/more/lib/instruction/assert
 
-export * from "./createTest.js"
-
 export * from "./expectMatch/index.js"
 export * from "./expectSpy/index.js"
